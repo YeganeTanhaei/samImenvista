e4a42ff92d34fb00b7d60f9621594e6694233ec4
Yegane Tanhaei <yeganetanhaei@gmail.com>
2026-08-28 19:49:52 +0330

    Configure Django for Render

--- samImenvista/settings.py
+++ samImenvista/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
 
 # Application definition
 
